596424cb789917061364886b53773ee481203eae
vantran1102 <67261528+vantran1102@users.noreply.github.com>
2025-11-10 14:20:14 -0800

    Time: 0 ms (100%), Space: 13.3 MB (61.79%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def search(self, nums, target):
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+        return -1
+        
