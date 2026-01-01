80aaa2d39a5ae2dd39252ab886073168f0e0b527
Van Tran <67261528+vantran1102@users.noreply.github.com>
2025-12-31 17:46:04 -0800

    Update 2134-minimum-swaps-to-group-all-1s-together-ii.py

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -3,9 +3,9 @@ class Solution:
         N = len(nums)
         total_ones = nums.count(1)
         l=0
-        window_ones = max_window_ones = 0
-        for r in range(2*N):
-            if nums[r%N]:
+        window_ones = max_window_ones = 0 #number of 1 currently inside window
+        for r in range(2*N): #2*N since array treated as circular 
+            if nums[r%N]==1:
                 window_ones+=1
             if r - l + 1 > total_ones:
                 window_ones -= nums[l%N]
