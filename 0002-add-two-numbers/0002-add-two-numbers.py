859594c09ff60415e0390e55be8b89212e3e1be5
Van Tran <67261528+vantran1102@users.noreply.github.com>
2026-01-13 19:49:57 -0800

    Time: 4 ms (52.6%), Space: 19.5 MB (5.25%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -19,8 +19,6 @@ class Solution:
             cur.next = ListNode(digit)
             cur = cur.next
             
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
         return dummy.next
