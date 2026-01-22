abce0b9784505507fd08395cbeb85d0ff53b12bb
Van Tran <67261528+vantran1102@users.noreply.github.com>
2026-01-21 16:08:52 -0800

    Time: 136 ms (59.05%), Space: 31.9 MB (53.75%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,17 +6,34 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+        #Option 1:
+        # count = 0
+        # if root is None:
+        #     return 0
+        # def dfs(node,max_node):
+        #     nonlocal count
+        #     if node is None:
+        #         return
+        #     if node.val >= max_node:
+        #         count+=1    
+        #     max_node = max(max_node, node.val)
+        #     dfs(node.left,max_node)
+        #     dfs(node.right,max_node)
+        # dfs(root, root.val)
+        # return count
+        #-----------------------------#
+        #Option 2:
+        def dfs(root,max_node,count):
+            if root is None:
+                return 0
+            if root.val >= max_node:
+                count+=1
+                max_node = root.val
+            if root.left is not None:
+                count += dfs(root.left,max_node,0)
+            if root.right is not None:
+                count += dfs(root.right,max_node,0)
+            return count
+        max_node = float("-inf")
         count = 0
-        if root is None:
-            return 0
-        def dfs(node,max_node):
-            nonlocal count
-            if node is None:
-                return
-            if node.val >= max_node:
-                count+=1    
-            max_node = max(max_node, node.val)
-            dfs(node.left,max_node)
-            dfs(node.right,max_node)
-        dfs(root, root.val)
-        return count
+        return dfs(root,max_node,count)
